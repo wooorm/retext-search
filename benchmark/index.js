9521e5ea449d1818f8936fc8cec1fe997be303cc
wooorm <tituswormer@gmail.com>
2014-09-26 14:20:48 +0200

    Refactor benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,57 +1,112 @@
 'use strict';
 
-var Retext, retext, search, source,
-    sourceTiny, sourceSmall, sourceMedium, sourceLarge,
-    tiny, small, medium, large;
+/**
+ * Dependencies.
+ */
 
-Retext = require('retext');
+var search,
+    Retext;
+
 search = require('..');
+Retext = require('retext');
 
-source = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ' +
+/**
+ * Fixtures.
+ */
+
+var tiny,
+    small,
+    medium,
+    large;
+
+tiny = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ' +
     'ac ultricies diam, quis vehicula mauris. Vivamus accumsan eleifend ' +
     'quam et varius. Etiam congue id magna eu fermentum. Aliquam mollis ' +
     'adipiscing.\n\n';
 
-/* Test data */
-sourceTiny = Array(11).join(source);
-sourceSmall = Array(11).join(sourceTiny);
-sourceMedium = Array(11).join(sourceSmall);
-sourceLarge = Array(11).join(sourceMedium);
+small = Array(11).join(tiny);
+medium = Array(11).join(small);
+large = Array(11).join(medium);
+
+/**
+ * Retext.
+ */
+
+var retext;
 
 retext = new Retext().use(search);
 
-tiny = retext.parse(sourceTiny);
-small = retext.parse(sourceSmall);
-medium = retext.parse(sourceMedium);
-large = retext.parse(sourceLarge);
+/**
+ * Benchmarks.
+ */
 
-/* Benchmarks */
-suite('Searching in lipsum for "Lorem"', function () {
-    bench('tiny (1 paragraph, 5 sentences, 30 words, 208 B)',
-        function (done) {
-            tiny.search('lorem');
-            done();
-        }
-    );
+suite('A section (10 paragraphs, 50 sentences, 300 words)', function () {
+    var tree;
 
-    bench('small (10 paragraphs, 50 sentences, 300 words, 2 kB)',
-        function (done) {
-            small.search('lorem');
-            done();
-        }
-    );
+    before(function (next) {
+        retext.parse(small, function (err, node) {
+            if (err) {
+                throw err;
+            }
 
-    bench('medium (100 paragraphs, 500 sentences, 3000 words, 21 kB)',
-        function (done) {
-            medium.search('lorem');
-            done();
-        }
-    );
+            tree = node;
+            next();
+        });
+    });
 
-    bench('large (1000 paragraphs, 5000 sentences, 30000 words, 208 kB)',
-        function (done) {
-            large.search('lorem');
-            done();
-        }
-    );
+    bench('Searching in lipsum for "Lorem"', function () {
+        tree.search('lorem');
+    });
+
+    bench('Searching parents in lipsum for "Lorem"', function () {
+        tree.searchAll('lorem');
+    });
 });
+
+suite('An article (100 paragraphs, 500 sentences, 3,000 words)', function () {
+    var tree;
+
+    before(function (next) {
+        retext.parse(medium, function (err, node) {
+            if (err) {
+                throw err;
+            }
+
+            tree = node;
+            next();
+        });
+    });
+
+    bench('Searching in lipsum for "Lorem"', function () {
+        tree.search('lorem');
+    });
+
+    bench('Searching parents in lipsum for "Lorem"', function () {
+        tree.searchAll('lorem');
+    });
+});
+
+suite('A book (1,000 paragraphs, 5,000 sentences, 30,000 words)',
+    function () {
+        var tree;
+
+        before(function (next) {
+            retext.parse(large, function (err, node) {
+                if (err) {
+                    throw err;
+                }
+
+                tree = node;
+                next();
+            });
+        });
+
+        bench('Searching in lipsum for "Lorem"', function () {
+            tree.search('lorem');
+        });
+
+        bench('Searching parents in lipsum for "Lorem"', function () {
+            tree.searchAll('lorem');
+        });
+    }
+);
